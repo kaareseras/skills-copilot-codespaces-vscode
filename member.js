338f91633679d66c03cb299f4cc311571a3e417b
kaareseras <kaare@seras.dk>
2023-08-10 11:14:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP'];
+    var member = {
+        name: 'Code Mafia',
+        old: 3,
+        skills: skills
+    };
+    console.log(member.skills[1]);
+}
